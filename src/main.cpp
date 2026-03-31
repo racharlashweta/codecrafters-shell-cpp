9ddfda64f6191a833196c3e2836826be87ee670b
Shweta Racharla <99076323+racharlashweta@users.noreply.github.com>
2026-03-31 10:33:22 +0000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,10 @@ int main() {
     std::getline(std::cin, input);
     if (input == "exit") {
       break;
+    } else if (input.substr(0, 5) == "echo ") {
+      std::cout << input.substr(5) << std::endl;
+    } else {
+      std::cout << input << ": command not found" << std::endl;
     }
-    std::cout << input << ": command not found" << std::endl;
   }
 }
